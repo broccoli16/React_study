eac01f2d002e53a4b1c09644b57271a5ac6a8360
broccoli16 <97226207+broccoli16@users.noreply.github.com>
2022-02-03 20:23:57 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ColorfullMessage from "./components/ColorfulMessage";
+
+const App = () => {
+  const onClickButton = () => alert();
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColorfullMessage color="blue"> お元気ですか？ </ColorfullMessage>
+      <ColorfullMessage color="pink"> 元気です！　</ColorfullMessage>
+
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
